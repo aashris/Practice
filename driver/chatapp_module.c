61b62dd7033c14e0c084ce14d42ec55a242caf51
Aashri <aashris@gmail.com>
2020-10-02 14:21:00 -0700

    Adding more formatting changes

--- driver/chatapp_module.c
+++ driver/chatapp_module.c
@@ -1,3 +1,4 @@
+// SPDX-License-Identifier: GPL-2.0
 /* kernel module for chat application */
 #include <linux/init.h>
 #include <linux/module.h>
@@ -28,7 +29,7 @@ static int chatapp_dev_release(struct inode *, struct file *);
 static ssize_t chatapp_dev_read(struct file *, char *, size_t, loff_t *);
 static ssize_t chatapp_dev_write(struct file *, const char *, size_t, loff_t *);
 
-static struct file_operations fops = {
+static const struct file_operations fops = {
 	.open = chatapp_dev_open,
 	.read = chatapp_dev_read,
 	.write = chatapp_dev_write,
@@ -37,23 +38,23 @@ static struct file_operations fops = {
 
 static int chatapp_init(void)
 {
-	printk(KERN_INFO "Registering chatapp by Aashri");
+	dev_info("Registering chatapp by Aashri");
 	major_number = register_chrdev(0, DEVICE_NAME, &fops);
 	if (major_number < 0) {
-		printk(KERN_ALERT "Chatapp failed to register a major number\n");
+		dev_alert("Chatapp failed to register a major number\n");
 		return major_number;
 	}
-	printk(KERN_INFO "Chatapp: registered correctly with major# %d\n",
+	dev_info("Chatapp: registered correctly with major# %d\n",
 		major_number);
 
 	// Register the device class
 	chatappClass = class_create(THIS_MODULE, CLASS_NAME);
 	if (IS_ERR(chatappClass)) {
 		unregister_chrdev(major_number, DEVICE_NAME);
-		printk(KERN_ALERT "Failed to register device class\n");
+		dev_alert("Failed to register device class\n");
 		return PTR_ERR(chatappClass);
 	}
-	printk(KERN_INFO "Chatapp: device class registered correctly\n");
+	dev_info("Chatapp: device class registered correctly\n");
 
 	// Register the device driver
 	chatappDevice = device_create(chatappClass, NULL,
@@ -61,10 +62,10 @@ static int chatapp_init(void)
 	if (IS_ERR(chatappDevice)) {
 		class_destroy(chatappClass);
 		unregister_chrdev(major_number, DEVICE_NAME);
-		printk(KERN_ALERT "Failed to create the device\n");
+		dev_alert("Failed to create the device\n");
 		return PTR_ERR(chatappDevice);
 	}
-	printk(KERN_INFO "Chatapp: device driver registered correctly\n");
+	dev_info("Chatapp: device driver registered correctly\n");
 	return 0;
 }
 
@@ -75,19 +76,19 @@ static void chatapp_exit(void)
 	class_unregister(chatappClass);
 	class_destroy(chatappClass);
 	unregister_chrdev(major_number, DEVICE_NAME);
-	printk(KERN_INFO "Chatapp: Goodbye from the LKM!\n");
+	dev_info("Chatapp: Goodbye from the LKM!\n");
 }
 
 static int chatapp_dev_open(struct inode *inodep, struct file *filep)
 {
 	numofusers++;
-	printk(KERN_INFO "Chatapp: User %d added to the chat\n", numofusers);
+	dev_info("Chatapp: User %d added to the chat\n", numofusers);
 	return 0;
 }
 
 static int chatapp_dev_release(struct inode *inodep, struct file *filep)
 {
-	printk(KERN_INFO "Chatapp: Closing chat\n");
+	dev_info("Chatapp: Closing chat\n");
 	numofusers--;
 	return 0;
 }
@@ -102,13 +103,12 @@ static ssize_t chatapp_dev_read(struct file *filep,
 	error_count = copy_to_user(buffer, msg, size_of_message);
 
 	if (error_count == 0) {            // if true then have success
-		printk(KERN_INFO "Chatapp: Sent %d characters to the user\n",
+		dev_info("Chatapp: Sent %d characters to the user\n",
 			size_of_message);
 		return (size_of_message = 0);
 	}
 
-	printk(KERN_INFO
-	"Chatapp: Failed to send %d characters to the user\n", error_count);
+	dev_alert("Chatapp: Failed to send %d chars\n", error_count);
 	return -EFAULT;
 }
 
@@ -116,11 +116,11 @@ static ssize_t chatapp_dev_write(struct file *filep, const char *buffer,
 					size_t len, loff_t *offset)
 {
 	if (copy_from_user(msg, buffer, len)) {
-		printk(KERN_ERR "Unable to copy from user");
+		dev_alert("Unable to copy from user");
 		return -1;
 	}
 	add_to_chat(msg, len);
-	printk(KERN_INFO "Chatapp: Received %zu characters from the user\n",
+	dev_info("Chatapp: Received %zu characters from the user\n",
 		len);
 	return len;
 }
--- driver/chatapp_rwhelper.c
+++ driver/chatapp_rwhelper.c
@@ -1,9 +1,10 @@
-#include <linux/init.h>             // Macros used to mark up functions e.g. __init __exit
-#include <linux/module.h>           // Core header for loading LKMs into the kernel
-#include <linux/device.h>           // Header to support the kernel Driver Model
-#include <linux/kernel.h>           // Contains types, macros, functions for the kernel
-#include <linux/fs.h>               // Header for the Linux file system support
-#include <linux/uaccess.h>            // Required for the copy to user function
+// SPDX-License-Identifier: GPL-2.0
+#include <linux/init.h>
+#include <linux/module.h>
+#include <linux/device.h>
+#include <linux/kernel.h>
+#include <linux/fs.h>
+#include <linux/uaccess.h>
 #include <linux/list.h>
 #include <linux/slab.h>
 
@@ -11,40 +12,43 @@
 
 LIST_HEAD(chat_linkedlist);
 
-typedef struct user_chat {
+struct chat {
 	char message[256];
 	struct list_head list;
-} chat;
+};
 
-void init_chat(void) {
-        LIST_HEAD(chat_linkedlist);
+void init_chat(void)
+{
+	LIST_HEAD(chat_linkedlist);
 }
 
-int add_to_chat(char *buffer, size_t len) {
-        
-        chat *ptr;
-        chat *input_chat = kmalloc(sizeof(*input_chat), GFP_KERNEL);
-        if (input_chat == NULL) {
-                printk(KERN_ERR "Unable to allocate memory for chat\n");
-                return -1;
-        }
-        snprintf(input_chat->message, len,  buffer);
-        printk(KERN_INFO "Input is %s\n", buffer);
-        INIT_LIST_HEAD(&input_chat->list);
+int add_to_chat(char *buffer, size_t len)
+{
+	struct chat *ptr;
+	struct chat *input_chat = kmalloc(sizeof(*input_chat), GFP_KERNEL);
 
-        list_add_tail(&input_chat->list, &chat_linkedlist);
-        
-        list_for_each_entry(ptr, &chat_linkedlist, list) {
-                printk(KERN_INFO "MSG: %s", ptr->message);
-        }
-        return 0;
+	if (input_chat == NULL)
+		return -1;
+
+	snprintf(input_chat->message, len,  buffer);
+	dev_info("Input is %s\n", buffer);
+	INIT_LIST_HEAD(&input_chat->list);
+
+	list_add_tail(&input_chat->list, &chat_linkedlist);
+
+	list_for_each_entry(ptr, &chat_linkedlist, list) {
+		dev_info("MSG: %s", ptr->message);
+	}
+	return 0;
 }
 
-void read_from_chat(void) {
-        chat *ptr;
-        list_for_each_entry(ptr, &chat_linkedlist, list) {
-                printk(KERN_INFO "MSG: %s", ptr->message);
-        }
-        
-        printk(KERN_INFO "End of list");
+void read_from_chat(void)
+{
+	struct chat *ptr;
+
+	list_for_each_entry(ptr, &chat_linkedlist, list) {
+		dev_info("MSG: %s", ptr->message);
+	}
+
+	dev_info("End of list");
 }
